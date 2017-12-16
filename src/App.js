311dbb9b2934ae38fa6cc2ecd75bc05abb826912
John Zhou <john.km.zhou@gmail.com>
2017-12-15 22:13:19 -1000

    Add ability to add, display, and remove items from firebase.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentItem: '',
+      username: '',
+      items: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    const itemsRef = firebase.database().ref('items');
+    itemsRef.on('value', (snapshot) => {
+      const items = snapshot.val();
+      let newState = [];
+
+      for (let item in items) {
+        newState.push({
+          id: item,
+          title: items[item].title,
+          user: items[item].user
+        });
+      }
+
+      this.setState({
+        items: newState
+      });
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const itemsRef = firebase.database().ref('items');
+    const item = {
+      title: this.state.currentItem,
+      user: this.state.username
+    }
+    itemsRef.push(item);
+    this.setState({
+      currentItem: '',
+      username: ''
+    });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  removeItem(itemId) {
+    const itemRef = firebase.database().ref(`/items/${itemId}`);
+    itemRef.remove();
+  }
+
   render() {
     return (
       <div className='app'>
@@ -13,15 +69,24 @@ class App extends Component {
         </header>
         <div className='container'>
           <section className='add-item'>
-            <form>
-              <input type="text" name="username" placeholder="What's your name?" />
-              <input type="text" name="currentItem" placeholder="What are you bringing?" />
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={this.state.username} />
+              <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={this.state.currentItem} />
               <button>Add Item</button>
             </form>
           </section>
           <section className='display-item'>
             <div className='wrapper'>
               <ul>
+                {this.state.items.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <h3>{item.title}</h3>
+                      <p>brought by: {item.user}</p>
+                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                    </li>
+                  )
+                })}
               </ul>
             </div>
           </section>
